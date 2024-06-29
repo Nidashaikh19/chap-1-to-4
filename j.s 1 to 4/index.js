f6c72f3ee05467dbbf37dfadbe79fbb26d22cefb
Nida <159054421+Nidashaikh19@users.noreply.github.com>
2024-06-29 10:14:48 +0500

    first commit

---
+++ j.s 1 to 4/index.js
@@ -0,0 +1,75 @@
+// .............chap : 1...........
+alert("Error! Please enter a valid password.");
+
+alert("Welcome to JS Land... Happy Coding!");
+
+alert("Welcome toJS Land...");
+
+alert("Happy Coding!");
+
+alert("Hello...! I can run JS through my web browser's console.");
+
+// ..........chap : 2 ..............
+
+var userName = "Jhone Doe";
+alert(userName);
+
+var years = "15 years old";
+alert(years);
+
+var course = "Certified Mobile Application Development";
+alert(course);
+
+var pizzaVeriable = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizzaVeriable);
+
+var email = "My email address is example@example.come";
+alert(email);
+
+var page = "Yah! I can write HTML content JavaScript. "
+document.write("<h1>" + page + "</h1>");
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
+
+// ...........chap : 3................
+
+var age = "Iam 15 years old";
+alert(age);
+
+var noOfVisit = "you have visited this site 14 times.";
+alert(noOfVisit);
+
+var birthYear = "My birth year is 1990 \n Datatype of my ddeclared variable is number ";
+document.write("<p>" + birthYear + "</p>");
+
+
+// ...........chap:4..........
+
+// ........declearation of variable ....
+var studentName = "Nida";
+studentAge = "22 years";
+gender = "female"
+city = "karachi";
+alert(studentName + "  " + "\n" + "  " + studentAge + "  " + "\n" + "  " + gender + "  " + "\n" + "  " + city)
+
+// .......legal.......
+/*var nameEmail
+var _2253
+var name
+var fullName
+var $price*/
+
+//  .......illegal....
+
+/*var 12city
+var fav food
+var @MediaList*/
+
+var heading = " “Rules for naming JS variables”";
+document.write("<h1>" + heading + "</h2>");
+document.write("Variable names can only contain letter,numbers, underscore and dollar sign. <br>")
+document.write("Variables must begin with a letter , underscore, or dollar sign.<br>")
+document.write("Variable names are case sensetive.<br>")
+document.write("Variable names should not be JavaScript keyword")
+
